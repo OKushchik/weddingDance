8efa3ced6822ba8ba75f5cf167f5d95f02d7d26f
Oksana Kushchik <okushchik@gmail.com>
2020-11-24 21:30:16 +0200

    first commit

---
+++ dist/js/slick-responsive.js
@@ -0,0 +1,135 @@
+// const { reload } = require("browser-sync");
+
+$(window).on('load resize orientationchange', function() {
+  if ($('.carousel').hasClass('slick-initialized') === false && $(window).width() < 824) {
+    $('.carousel').slick({
+      dots: true,
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      mobileFirst: true,
+      arrows: false,
+      centerMode: true,
+      slidesToShow: 1,
+      autoplay: false,
+      adaptiveHeight: true,
+      responsive: [
+        {
+          breakpoint: 640,
+          settings: "unslick"
+        },
+      ]
+    });
+  }
+})
+
+
+
+
+$('.carousel-2').slick({
+  dots: true,
+  arrows: true,
+  infinite: false,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  centerMode: false,
+  centerPadding: '50px',
+  autoplay: false,
+  adaptiveHeight: true,
+  responsive: [
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        slidesToShow: 1,
+        adaptiveHeight: true, 
+      }
+    }
+  ]
+});
+
+
+//imitation click on errows
+  let arrowsPrev = document.querySelector('.arrows__prev');
+  let arrowsNext = document.querySelector('.arrows__next');
+  function imitErrows (){
+    let slickPrev = document.querySelector('.slick-prev');
+    let slickNext = document.querySelector('.slick-next');
+
+    arrowsPrev.addEventListener('click', getObj.bind(null,slickPrev))
+    arrowsNext.addEventListener('click', getObj.bind(null,slickNext))
+
+    function getObj (getObj) {
+      let event = new Event("click");
+      getObj.dispatchEvent(event);
+    }
+  }
+imitErrows();
+
+// errowsInfo
+let current = document.querySelector('.couches .slick-current');
+let countOfCouchesBlock = Math.ceil(+document.querySelectorAll('.couch-card').length/2);
+let currentIndex = Math.ceil((+current.getAttribute('data-slick-index') + 1)/2);
+let arrowsInform = document.querySelector('.arrows__inform');
+let transform = document.querySelector('.slick-track');
+
+arrowsInform.textContent = `${currentIndex} of ${countOfCouchesBlock}`
+
+function changArrowInfo (){
+  current = document.querySelector('.couches .slick-current');
+  currentIndex = Math.ceil((+current.getAttribute('data-slick-index') + 1)/2);
+  arrowsInform.textContent = `${currentIndex} of ${countOfCouchesBlock}`
+}
+arrowsNext.addEventListener('click', function(){
+  changArrowInfo()
+})
+arrowsPrev.addEventListener('click', function(){
+  changArrowInfo ()
+})
+transform.onclick = function(){
+  changArrowInfo ()
+}
+
+
+
+
+// errowsInfo
+// function infoErrow (count = 0){
+//   console.log(count)
+//   if ($(window).width() > 420) {
+//     count = Math.ceil(count/2)
+//   }
+  
+//   let slickDots = document.querySelectorAll('.couches ul.slick-dots li button');
+//   let arrowsInform = document.querySelector('.arrows__inform');
+
+// arrowsInform.textContent = slickDots[count].getAttribute('aria-label')
+//   arrowsNext.addEventListener('click', function(){
+//     if (count<slickDots.length-1){
+//       count ++;
+//     } else {
+//       count = slickDots.length-1;
+//     }
+//   arrowsInform.textContent = slickDots[count].getAttribute('aria-label')
+//   })
+//   arrowsPrev.addEventListener('click', function(){
+//     if (count>0){
+//       count --;
+//     } else {
+//       count = 0;
+//     }
+//     arrowsInform.textContent = slickDots[count].getAttribute('aria-label')
+//   })
+// }
+// infoErrow()
+
+// $(window).on('resize', function() {
+//   let current = document.querySelector('.couches .slick-current');
+//   let currentIndex = current.getAttribute('data-slick-index')
+//   imitErrows();
+//   infoErrow(currentIndex)
+// })
+
+
---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    concat       = require ('gulp-concat'),
+    rename       = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    watch        = require('gulp-watch'),
+    iconfont     =   require( 'gulp-iconfont' ),
+    browserSync  = require('browser-sync').create(),
+    imagemin     = require('gulp-imagemin');
+    iconfontCss  =   require( 'gulp-iconfont-css' );
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        notify: false,
+        tunnel: true,
+        online: true
+    });
+});
+    
+//imagesMin
+gulp.task('imagemin', async () => {
+    gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+});
+
+//styles (compressed, autoprefixer)
+gulp.task('style', gulp.series(function () {
+    return gulp.src('src/styles/style.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+}));
+
+//styles (watch)
+gulp.task('watch', function () {
+    return gulp.watch('src/styles/**/*.scss', gulp.series('default'));
+});
+
+gulp.task('default', gulp.series('style'));
+
+
+//icon fonts
+var fontName = 'icons';
+
+//add svg icons to the folder "icons" and use 'iconfont' task for generating icon font
+gulp.task( 'iconfont', async () => {
+    gulp.src( 'dist/assets/icons/*.svg' )
+        .pipe( iconfontCss( {
+            // где будет наш scss файл
+            targetPath   : '../../../src/styles/common/_icons.scss',
+            // пути подлючения шрифтов в _icons.scss
+            fontPath     : '../../../dist/assets/fontIcons/',
+            fontName    : fontName
+
+        } ) )
+        .pipe( iconfont( {
+            fontName    : fontName,
+            formats     : [ 'svg', 'ttf', 'eot', 'woff', 'woff2' ],
+            normalize   : true,
+            fontHeight  : 1001
+        } ) )
+        .pipe( gulp.dest( 'dist/assets/fontIcons' ) )
+});

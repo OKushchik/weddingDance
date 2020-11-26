47d0af7f8d3b0c4e25af425d4016afaf528dd29e
Oksana Kushchik <okushchik@gmail.com>
2020-11-26 18:58:39 +0200

    add variable

--- dist/js/slick-responsive.js
+++ dist/js/slick-responsive.js
@@ -1,4 +1,3 @@
-// const { reload } = require("browser-sync");
 
 $(window).on('load resize orientationchange', function() {
   if ($('.carousel').hasClass('slick-initialized') === false && $(window).width() < 824) {
@@ -21,31 +20,6 @@ $(window).on('load resize orientationchange', function() {
     });
   }
 })
-
-// $('.carousel').slick({
-//   infinite: false,
-//   slidesToShow: 3,
-//   slidesToScroll: 3,
-//   arrows: false,
-//   adaptiveHeight: true,
-//   responsive: [
-//     {
-//       breakpoint: 640,
-//       settings: {
-//         initialSlide: 1,
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         centerMode: true,
-//         adaptiveHeight: true,
-//         dots: true,
-//       }
-//     },
-//   ]
-// });
-
-
-
-
 
 $('.carousel-2').slick({
   dots: true,
@@ -72,24 +46,9 @@ $('.carousel-2').slick({
 });
 
 
-// //imitation click on errows
-  let arrowsPrev = document.querySelector('.arrows__prev'); 
-  let arrowsNext = document.querySelector('.arrows__next'); 
-//   function imitErrows (){
-//     let slickPrev = document.querySelector('.slick-prev');
-//     let slickNext = document.querySelector('.slick-next');
- 
-//     arrowsPrev.addEventListener('click', getObj.bind(null,slickPrev))
-//     arrowsNext.addEventListener('click', getObj.bind(null,slickNext))
-
-//     function getObj (getObj) {
-//       let event = new Event("click");
-//       getObj.dispatchEvent(event);
-//     }
-//   }
-// imitErrows();
-
 // errowsInfo
+let arrowsPrev = document.querySelector('.arrows__prev'); 
+let arrowsNext = document.querySelector('.arrows__next'); 
 let arrowsInform = document.querySelector('.arrows__inform');
 let transform = document.querySelector('.couches__section');
 
@@ -121,20 +80,10 @@ transform.addEventListener ('mouseup', function(){
 })
 
 
-/////
+/////Popap animation
 const element = document.querySelector('.burger-popup');
 const elementOpen = document.querySelector('.burger-icon.burger-open');
 const elementClose = document.querySelector('.burger-icon.burger-close');
-
-// elementOpen.addEventListener('click', function(){
-//   if(element.classList.contains('animate__fadeInDownBig')) {
-//     element.classList.remove('animate__animated', 'animate__fadeInDownBig')
-//   } else if (element.classList.contains('animate__fadeOutUpBig')){
-//     element.classList.remove('animate__animated', 'animate__fadeOutUpBig')
-//   } else {
-//     element.classList.add('animate__animated', `${typeOfAnimation}`)
-//   }
-// })
 
 elementOpen.addEventListener('click', addAnimation.bind(null,'animate__fadeInDownBig','animate__fast'))
 elementClose.addEventListener('click', addAnimation.bind(null,'animate__fadeOutUpBig','animate__slow'))
@@ -152,16 +101,4 @@ function addAnimation (typeOfAnimation, slow) {
      element.classList.add('animate__animated', `${typeOfAnimation}`,`${slow}`) 
 
 }
-
-
-// $(window).on('load', function() {
-//   setTimeout(function(){ 
-
-//     // document.querySelector('.show-case__text').style.display = 'block';
-//     document.querySelector('.show-case__img').classList.add('changeSizeImg');
-//     document.querySelector('.show-case__text').classList.add('changeSizeTitle');
-
-// }, 1000);
-
-// })
 
